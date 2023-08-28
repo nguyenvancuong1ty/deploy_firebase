c73b444fdd402a62644d6d7b0130f2f08d7053db
Cuong Nguyen 2k1 <nguyenvancuong19032002@gmail.com>
2023-08-28 17:43:50 +0700

    ..

--- BE/src/dist/server.js
+++ BE/src/dist/server.js
@@ -1,25 +1,29 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
+'use strict';
+var __importDefault =
+    (this && this.__importDefault) ||
+    function (mod) {
+        return mod && mod.__esModule ? mod : { default: mod };
+    };
+Object.defineProperty(exports, '__esModule', { value: true });
+const express_1 = __importDefault(require('express'));
+const cors_1 = __importDefault(require('cors'));
 require('dotenv').config();
 // const db = require("./firebase");
-const helmet_1 = __importDefault(require("helmet"));
-const route_product_1 = __importDefault(require("./src/routes/route.product"));
-const route_account_1 = __importDefault(require("./src/routes/route.account"));
-const route_cart_1 = __importDefault(require("./src/routes/route.cart"));
-const route_order_1 = __importDefault(require("./src/routes/route.order"));
+const helmet_1 = __importDefault(require('helmet'));
+const route_product_1 = __importDefault(require('./src/routes/route.product'));
+const route_account_1 = __importDefault(require('./src/routes/route.account'));
+const route_cart_1 = __importDefault(require('./src/routes/route.cart'));
+const route_order_1 = __importDefault(require('./src/routes/route.order'));
 const app = (0, express_1.default)();
 const port = process.env.PORT;
 app.use((0, helmet_1.default)());
 app.use(express_1.default.static('public'));
-app.use((0, cors_1.default)({
-    origin: ['http://localhost:3006', 'https://cuongcodedao.id.vn', /myfirebase\.com$/, /cakes\.com$/],
-    credentials: true,
-}));
+app.use(
+    (0, cors_1.default)({
+        origin: ['http://localhost:3006', 'https://deploy-firebase-fe.vercel.app', 'https://cuongcodedao.id.vn', /myfirebase\.com$/, /cakes\.com$/],
+        credentials: true,
+    }),
+);
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use('/', route_account_1.default);
