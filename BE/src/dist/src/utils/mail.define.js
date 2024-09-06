d91c8a8a068d4486692b00694438b8dda679714a
dev6sinka <76762392+dev6sinka@users.noreply.github.com>
2024-09-06 17:15:30 +0700

    update authentication send mail

--- BE/src/dist/src/utils/mail.define.js
+++ BE/src/dist/src/utils/mail.define.js
@@ -10,21 +10,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.mailDefine = void 0;
-const auth2_0_1 = require("./auth2.0");
 const nodemailer = require('nodemailer');
 const mailDefine = () => __awaiter(void 0, void 0, void 0, function* () {
-    const accessToken = yield (0, auth2_0_1.getAccessToken)();
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         port: 587,
         secure: false,
         auth: {
-            type: 'OAuth2',
             user: 'nguyenvancuong19032002@gmail.com',
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            refreshToken: process.env.REFRESH_TOKEN,
-            accessToken: accessToken,
+            pass: process.env.PASSMAIL,
         },
     });
     return transporter;
